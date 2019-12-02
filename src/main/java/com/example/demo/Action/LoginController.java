a1f44fabc3b854cd0edad61239af54767d8917e1
admin <564059064@qq.om>
2019-12-02 14:35:03 +0800

    暂时登录功能修改

--- src/main/java/com/example/demo/Action/LoginController.java
+++ src/main/java/com/example/demo/Action/LoginController.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -16,14 +18,19 @@ public class LoginController{
     @Autowired
     private UserMapper userMapper;
 
+    @ResponseBody
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(@RequestParam("username") String username,@RequestParam("pwd")String pwd,
-                        HttpServletRequest request){
-        request.getParameter("Code");
-        List<User> user = userMapper.login(username,pwd);
-        if(user != null){
-            return "success";
+                        HttpServletRequest request) throws IOException{
+        String code = request.getParameter("code").toUpperCase();
+        String imgCode = request.getSession().getAttribute("randomcode_key").toString().toUpperCase();
+        boolean flag = false;
+        if(code.equals(imgCode)){
+            List<User> user = userMapper.login(username,pwd);
+            if(user != null){
+                return "ture";
+            }
         }
-        return null;
+        return "false";
     }
 }
